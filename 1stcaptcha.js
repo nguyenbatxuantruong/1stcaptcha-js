282d2ff2de57b0b50c2872fa6b83aa95ab37ae71
truongnbx <nguyenbatxuantruong@gmail.com>
2023-09-18 17:33:30 +0700

    first commit

---
+++ 1stcaptcha.js
@@ -0,0 +1,272 @@
+class OneStCaptchaClient {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = "https://api.1stcaptcha.com";
+  }
+
+  async getBalance() {
+    const response = await fetch(this.baseURL + "/user/balance?" +
+      new URLSearchParams({
+        apikey: this.apiKey,
+      })
+    );
+    let data = await response.json();
+    if (response.status === 200) {
+      if (data["Code"] === 0) {
+        return data["Balance"]
+      } else {
+        throw new Error(`${data}`);
+      }
+    } else {
+      throw new Error(`${await response.json()}`);
+    }
+  }
+
+  async getTaskResult(taskId, timeoutSec = 180, timeSleep = 2, typeCaptcha = "") {
+    let timeStart = new Date().getTime();
+    let timeEnd = timeStart + timeoutSec * 1000;
+    while (new Date().getTime() < timeEnd) {
+      const response = await fetch(this.baseURL + "/getresult?" +
+        new URLSearchParams({
+          apikey: this.apiKey, taskid: taskId,
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          if (data['Status'] === "SUCCESS") {
+            if (typeCaptcha === "image2text" || typeCaptcha === "recaptcha_click") {
+              return data["Data"]
+            }
+            if (typeCaptcha === "v3_enterprise") {
+              return {
+                token: data["Data"]["Token"],
+                userAgent: data["Data"]["UserAgent"],
+              }
+            }
+            return data["Data"]["Token"]
+          } else if (data['Status'] === "ERROR") {
+            throw new Error(`${data["Message"]}`);
+          }
+          // Wait timeSleep seconds
+          await new Promise(r => setTimeout(r, timeSleep * 1000));
+        } else {
+          throw new Error(`${data["Error"]}`);
+        }
+      } else {
+        throw new Error(`${await response.json()}`);
+      }
+    }
+  }
+
+  async solveRecaptchaV2ProxyLess(siteUrl, siteKey, invisible = false, timeout = 180, timeSleep = 2) {
+    try {
+      const response = await fetch(this.baseURL + "/recaptchav2?" +
+        new URLSearchParams({
+          apikey: this.apiKey,
+          sitekey: siteKey,
+          siteurl: siteUrl,
+          version: "v2",
+          invisible: invisible.toString(),
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          let taskId = data['TaskId'];
+          let token = await this.getTaskResult(taskId, timeout, timeSleep);
+          return {
+            code: 0,
+            token: token,
+          }
+        }
+      }
+      return { "code": 1, "messeage": data }
+    } catch (e) {
+      return { "code": 1, "messeage": e.toString() }
+    }
+
+  }
+
+  async solveRecaptchaV2EnterpriseProxyLess(siteUrl, siteKey, timeout = 180, timeSleep = 2) {
+    try {
+      const response = await fetch(this.baseURL + "/recaptchav2_enterprise?" +
+        new URLSearchParams({
+          apikey: this.apiKey,
+          sitekey: siteKey,
+          siteurl: siteUrl,
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          let taskId = data['TaskId'];
+          let token = await this.getTaskResult(taskId, timeout, timeSleep);
+          return {
+            code: 0,
+            token: token,
+          }
+        }
+      }
+      return { "code": 1, "messeage": data }
+    } catch (e) {
+      return { "code": 1, "messeage": e.toString() }
+    }
+
+  }
+
+  async solveRecaptchaV3ProxyLess(siteUrl, siteKey, pageAction, minScore = 0.3, timeout = 180, timeSleep = 2) {
+    try {
+      const response = await fetch(this.baseURL + "/recaptchav3?" +
+        new URLSearchParams({
+          apikey: this.apiKey,
+          sitekey: siteKey,
+          siteurl: siteUrl,
+          pageaction: pageAction,
+          minscore: minScore,
+          version: "v3",
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          let taskId = data['TaskId'];
+          let token = await this.getTaskResult(taskId, timeout, timeSleep);
+          return {
+            code: 0,
+            token: token,
+          }
+        }
+      }
+      return { "code": 1, "messeage": data }
+    } catch (e) {
+      return { "code": 1, "messeage": e.toString() }
+    }
+  }
+
+  async solveRecaptchaV3EnterpriseProxyLess(siteUrl, siteKey, pageAction, minScore = 0.3, timeout = 180, timeSleep = 2) {
+    try {
+      const response = await fetch(this.baseURL + "/recaptchav3_enterprise?" +
+        new URLSearchParams({
+          apikey: this.apiKey,
+          sitekey: siteKey,
+          siteurl: siteUrl,
+          pageaction: pageAction,
+          minscore: minScore,
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          let taskId = data['TaskId'];
+          let result = await this.getTaskResult(taskId, timeout, timeSleep, "v3_enterprise");
+          return {
+            code: 0,
+            token: result.token,
+            userAgent: result?.userAgent,
+          }
+        }
+      }
+      return { "code": 1, "messeage": data }
+    } catch (e) {
+      return { "code": 1, "messeage": e.toString() }
+    }
+  }
+
+  async funCaptchaProxyLess(siteUrl, siteKey, timeout = 180, timeSleep = 3) {
+    try {
+      const response = await fetch(this.baseURL + "/funcaptchatokentask?" +
+        new URLSearchParams({
+          apikey: this.apiKey,
+          sitekey: siteKey,
+          siteurl: siteUrl,
+        })
+      );
+      let data = await response.json();
+      if (response.status === 200) {
+        if (data["Code"] === 0) {
+          let taskId = data['TaskId'];
+          let token = await this.getTaskResult(taskId, timeout, timeSleep);
+          return {
+            code: 0,
+            token: token,
+          }
+        }
+      }
+      return { "code": 1, "messeage": data }
+    } catch (e) {
+      return { "code": 1, "messeage": e.toString() }
+    }
+  }
+
+  async recaptchaClick(urlList, caption, timeout = 60, timeSleep = 3) {
+    try {
+      const response = await fetch(this.baseURL + "/recognition", {
+        method: "POST",
+        body: JSON.stringify({
+          Image_urls: urlList,
+          Caption: caption,
+          Apikey: this.apiKey,
+          Type: "recaptcha",
+        })
+    })
+    let data = await response.json();
+    if (response.status === 200) {
+      if (data["Code"] === 0) {
+        let taskId = data['TaskId'];
+        let token = await this.getTaskResult(taskId, timeout, timeSleep, "recaptcha_click");
+        return {
+          code: 0,
+          token: token,
+        }
+      }
+    }
+    return { "code": 1, "messeage": data }
+  } catch(e) {
+    return { "code": 1, "messeage": e.toString() }
+  }
+}
+
+  async convertFileToBase64(filePath) {
+  const file = await fetch(filePath);
+  return new Promise(async (resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(await file.blob());
+    reader.onload = () => resolve(reader.result.toString().replace(/^data:(.*,)?/, ''));
+    reader.onerror = reject;
+  });
+}
+
+  async imageToText(base64img = undefined, filePath = undefined, timeout = 60, timeSleep = 3) {
+  if (!base64img && !filePath) {
+    return { "code": 1, "messeage": "base64img or filePath is required" }
+  }
+  if (!base64img) {
+    base64img = await this.convertFileToBase64(filePath)
+  }
+  try {
+    const response = await fetch(this.baseURL + "/recognition", {
+      method: "POST",
+      body: JSON.stringify({
+        Image: base64img,
+        Apikey: this.apiKey,
+        Type: "imagetotext",
+      })
+    })
+    let data = await response.json();
+    if (response.status === 200) {
+      if (data["Code"] === 0) {
+        let taskId = data['TaskId'];
+        let token = await this.getTaskResult(taskId, timeout, timeSleep, "image2text");
+        return {
+          code: 0,
+          token: token,
+        }
+      }
+    }
+    return { "code": 1, "messeage": data }
+  } catch (e) {
+    return { "code": 1, "messeage": e.toString() }
+  }
+}
+}
---
+++ readme.md
@@ -0,0 +1,133 @@
+1stCaptcha package for JS
+=
+[1stcaptcha.com](https://1stcaptcha.com) package for JS
+
+Solver recaptchaV2, recaptchaV3, hcaptcha, funcaptcha, imageToText, Zalo Captcha,.... Super fast and cheapest.
+
+# Install
+
+## CDN install
+
+```html
+<head>
+    <script src="{{URL_PATH}}/1stcaptcha.js"></script>
+</head>
+```
+
+## Manual install
+
+```
+download github.com/1stcaptcha/1stcaptcha-js
+add library to script
+```
+```html
+<head>
+    <script src="{{FILE_PATH}}/1stcaptcha.js"></script>
+</head>
+```
+
+# Usage
+
+## init client
+
+```html
+<script>
+  const apiKey = "YOUR_API_KEY";
+  const client = new OneStCaptchaClient(apiKey);
+</script>
+```
+
+## solver recaptcha v2:
+
+```node
+(async () => {
+  const result = await client.solveRecaptchaV2ProxyLess(
+    "https://www.google.com/recaptcha/api2/demo",
+    "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
+  );
+  console.log(result);
+})();
+```
+
+## solver recaptcha v2 enterprise:
+
+```node
+(async () => {
+  const result = await client.solveRecaptchaV2EnterpriseProxyLess(
+    "YOUR_SITE_URL",
+    "YOUR_SITE_KEY"
+  );
+  if (result.code === 0) {
+    console.log(result.token);
+  } else {
+    console.log(result.messeage);
+  }
+})()
+```
+
+## solver recaptcha v3:
+
+```node
+(async () => {
+  const result = await client.solveRecaptchaV3ProxyLess(
+    "YOUR_SITE_URL",
+    "YOUR_SITE_KEY",
+    "YOUR_ACTION"
+  );
+  console.log(result);
+})()
+```
+
+## solver recaptcha v3 enterprise:
+
+```node
+(async () => {
+  const result = await client.solveRecaptchaV3EnterpriseProxyLess("YOUR_SITE_URL", "YOUR_SITE_KEY", "YOUR_ACTION");
+  if (result.code === 0) {
+    console.log(result.token);
+    console.log(result.userAgent)
+  } else {
+    console.log(result.messeage);
+  }
+})()
+```
+
+## solve image2text
+
+```node
+(async () => {
+  const result = await client.imageToText(
+    "YOUR_BASE64_IMG",
+    "YOUR_IMG_URL"
+  );
+  console.log(result);
+})()
+```
+
+## solve recaptchaClick
+
+```node
+(async () => {
+  const result = await client.recaptchaClick(
+    "https://www.google.com/recaptcha/api2/payload?p=06AAYGu2SS-VoFh3lhBvNxEIR5EmdEoqpJ1JVtMOvqYII1DGQbfui2S_ela5OF3pO4pxfjMQxRkUMm2szwznSZNoWruONOUTUcbxwMn2mVimdFm6oX0_qAyQJ0EtjNSpOl_H4kFjMPOZIr2i11TKRTG447zgqw_XlhnvR3NKNtfOIZc0V1_4H_OvOAY6IwgYjaflJWHMIXMvNEnt7rOTpC3WPw4VfbkCx20w&k=6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-",
+    "Select all squares with bicycles"
+  );
+  console.log(result);
+})()
+```
+
+## funcaptcha
+
+```node
+(async () => {
+  const result = await client.funCaptchaProxyLess(
+    "https://outlook.com/",
+    "2CB16598-CB82-4CF7-B332-5990DB66F3AB"
+  );
+  if (result.code === 0) {
+    console.log(result.token);
+  } else {
+    console.log(result.messeage);
+  }
+})()
+```
